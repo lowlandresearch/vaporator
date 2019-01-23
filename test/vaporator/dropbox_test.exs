97ab71f7b91032d6b8c80827446753055d607459
Chaz Watkins <chazwatkins@live.com>
2019-01-22 23:07:34 -0600

    implement unit tests for Vaporator.Dropbox.post_api

--- test/vaporator/dropbox_test.exs
+++ test/vaporator/dropbox_test.exs
@@ -4,11 +4,62 @@ defmodule Vaporator.DropboxTest do
 
   @dbx %Vaporator.Dropbox{access_token: System.get_env("DROPBOX_ACCESS_TOKEN")}
 
-  # setup_all do
-  #   HTTPoison.start()
-  # end
-
   test "lists the root directory" do
     assert length(Map.keys(Vaporator.Cloud.list_folder(@dbx, "/"))) > 0
   end
+
+  test "sets header with Content-Type json" do
+    assert Vaporator.Dropbox.json_headers == %{"Content-Type" => "application/json"}
+  end
+
+  test "sets header with Authorization" do
+    assert Vaporator.Dropbox.headers(@dbx) == %{"Authorization" => "Bearer #{@dbx.access_token}"}
+  end
+
+  test "post_api: successful call" do
+    body = %{"path" => ""}
+    result = to_string(Poison.Encoder.encode(body, %{}))
+    response = Vaporator.Dropbox.post_api(
+      @dbx,
+      "/files/list_folder",
+      result
+    )
+    assert {:ok, _} = response
+  end
+
+  test "post_api: bad url supplied" do
+    body = %{"path" => ""}
+    result = to_string(Poison.Encoder.encode(body, %{}))
+    response = Vaporator.Dropbox.post_api(
+      @dbx,
+      "/bogus/url",
+      result
+    )
+    {:error,{_,_,{_,{_,reason}}}} = response
+    assert String.contains?(reason, "Unknown API")
+  end
+
+  test "post_api: No body supplied" do
+    body = %{"path" => ""}
+    result = to_string(Poison.Encoder.encode(body, %{}))
+    response = Vaporator.Dropbox.post_api(
+      @dbx,
+      "/files/list_folder"
+    )
+    {:error,{_,_,{_,{_,reason}}}} = response
+    assert String.contains?(reason, "request body")
+  end
+
+  test "post_api: Invalid OAuth token supplied" do
+    body = %{"path" => ""}
+    result = to_string(Poison.Encoder.encode(body, %{}))
+    response = Vaporator.Dropbox.post_api(
+      %Vaporator.Dropbox{access_token: "123456abcd"},
+      "/files/list_folder",
+      result
+    )
+    {:error,{_,_,{_,{_,reason}}}} = response
+    assert String.contains?(reason, "token is malformed")
+  end
+
 end
