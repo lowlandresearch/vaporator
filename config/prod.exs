890269ab73f44623ae7de377743afc7b7213079d
David O'Gwynn <dogwynn@acm.org>
2019-02-21 12:20:56 -0600

    separated into different environments

---
+++ config/prod.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :vaporator, clientfs_sync_dirs: []
+
+import_config "instance.exs"
---
+++ config/test.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+config :vaporator, cloudfs_root: "/vaporator/test/"
+
+config :vaporator, clientfs_sync_dirs: []
+
+config :vaporator, test_dir: "/vaporator/test/"
+config :vaporator, test_file: "test.txt"
+
+config :logger, level: :warn
+
+config :exvcr,
+  vcr_cassette_library_dir: "test/vaporator/fixture/vcr_cassettes",
+  custom_cassette_library_dir: "test/vaporator/fixture/custom_cassettes",
+  filter_sensitive_data: [
+    [pattern: "Bearer .+", placeholder: "<<DROPBOX_ACCESS_TOKEN>>"]
+  ],
+  filter_url_params: false,
+  filter_request_headers: [],
+  response_headers_blacklist: ["X-Dropbox-Request-Id"]
+
+import_config "instance.exs"
