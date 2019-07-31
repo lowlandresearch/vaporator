176a551a8cdf9e08caa77b635f84f291ea4720e6
Chaz Watkins <chaz.watkins@ericsson.com>
2019-07-31 17:53:29 -0500

    add ethernet check for status

--- firmware/lib/status_monitor.ex
+++ firmware/lib/status_monitor.ex
@@ -3,7 +3,7 @@ defmodule Firmware.StatusMonitor do
 
   require Logger
 
-  alias Nerves.Network
+  alias Nerves.{Network, Leds}
 
   @interval 500
 
@@ -18,12 +18,20 @@ defmodule Firmware.StatusMonitor do
     {:ok, state}
   end
 
-  defp wireless_interface_up? do
-    case Network.status("wlan") do
+  defp network_interface_up?(interface) do
+    case Network.status(interface) do
       %{is_up: true, operstate: :down} -> false
       %{is_up: false} -> false
       _ -> true
     end
+  end
+
+  defp ethernet_interface_up? do
+    network_interface_up?("eth0")
+  end
+
+  defp wireless_interface_up? do
+    network_interface_up?("wlan0")
   end
 
   defp internet_reachable? do
@@ -33,11 +41,11 @@ defmodule Firmware.StatusMonitor do
     )
   end
 
-  defp all_settings_present? do
-    filesync_settings_set?() and cloud_settings_set?()
+  defp filesync_settings_set? do
+    client_settings_set?() and cloud_settings_set?()
   end
 
-  defp filesync_settings_set? do
+  defp client_settings_set? do
     client = SettingStore.get(:client)
     client.sync_dirs != []
   end
@@ -47,19 +55,50 @@ defmodule Firmware.StatusMonitor do
     cloud.access_token != nil and cloud.root_path != nil
   end
 
+  @doc """
+  Checks if all required settings are set for the system to run.
+  
+  Returns `boolean`
+  """
+  def system_settings_set? do
+    filesync_settings_set?()
+  end
+
+  defp system_ok? do
+    system_settings_set?()
+    and ethernet_interface_up?()
+    and wireless_interface_up?()
+    and internet_reachable?()
+  end
+
+  @doc """
+  Gets the current system status settings for Nerves.Leds
+
+  Returns ```elixir
+          [connected: true, alert: false]
+          or
+          [connected: false, alert: true]
+          ```
+  """
+  def get_system_status do
+    if system_ok?() do
+      [connected: true, alert: false]
+    else
+      [connected: false, alert: true]
+    end
+  end
+
   # Server
 
   def handle_info(:monitor, state) do
-    ok? =
-      all_settings_present?()
-      and wireless_interface_up?()
-      and internet_reachable?()
 
-    Nerves.Leds.set(connected: ok?, alert: not ok?)
+    current_status = get_system_status()
+
+    Leds.set(current_status)
 
     Process.send_after(self(), :monitor, @interval)
 
-    {:noreply, state}
+    {:noreply, current_status}
   end
 
 end
